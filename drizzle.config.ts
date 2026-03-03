4eb9f511e8fbb6d27abd8dd27e365274eba29a20
Akhdan Yafi Widiyanto <akhdanyafi@MacBook-Air-Akhdan.local>
2026-03-03 17:07:29 +0700

    Fix Terbaru Bug

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     dbCredentials: {
         host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD,
+        password: process.env.DB_PASSWORD || '',
         database: process.env.DB_NAME || 'be-labuai',
         port: parseInt(process.env.DB_PORT || '3306'),
     },
